8f20473404bb4e4ae6d68c0dda8dfe8c21cf9080
jaumeferr <36743497+jaumeferr@users.noreply.github.com>
2020-04-15 16:53:14 +0200

    fase1 - configurar cartera
    Crear la base del html de la fase 1 + creacion una cartera de proyecto guardada en local storage

--- js/fase1Controller.js
+++ js/fase1Controller.js
@@ -1,6 +1,67 @@
 var userLogged;
+var carteraProyectos;
 
 $(document).ready(function() {
     userLogged = JSON.parse(localStorage.getItem('userLogged'));
     $("#userInfo").append("User: " + userLogged.user + "\n Role: " + userLogged.role);
+    var cartAux = localStorage.getItem('carteraProyectos');
+
+    if (cartAux) {
+        carteraProyectos = JSON.parse(cartAux);
+    } else {
+        /*Abrir archivo JSON on info inicial de la cartera
+        debugger;
+        var myInit = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors',
+            cache: 'default'
+        }
+        let myReq = new Request("./json/carteraProyectos.json", myInit);
+
+        fetch(myReq)
+            .then(function(resp) {
+                return resp.json();
+            })
+            .then(function(data) {
+                console.log(data);
+            })
+        */
+
+        //Alternativa, declarar aquí la cartera
+        carteraProyectos = {
+            config: {
+                rrff: {
+                    procesos: [],
+                    costeProcesos: "",
+                    costeRRHH: "",
+                    cuantiaInversion: ""
+                },
+                rrhh: [],
+                criterios: [],
+                calendario: {
+                    fechaPublicacionConvocatoria: "",
+                    periodoPresentacionPropuestas: {
+                        desde: "",
+                        hasta: ""
+                    },
+                    periodoEvaluacionPriorizacion: {
+                        desde: "",
+                        hasta: ""
+                    },
+                    fechaPublicacionAprobados: ""
+                },
+                documentacion: {
+                    descripcion: "",
+                    reglamento: "",
+                    formularioPropuesta: "",
+                    textoConvocatoria: ""
+                }
+            }
+        }
+
+        localStorage.setItem('carteraProyectos', JSON.stringify(carteraProyectos));
+    }
 });
