d52b7b5b48525fa2de1b37420e5688a25f704dda
jaumeferr <36743497+jaumeferr@users.noreply.github.com>
2020-04-19 01:48:44 +0200

    Añadir criterios
    Lista de criterios + ventana modal para añadir criterios.  Falta borrar la info de los inputs al salir de la modal.

--- js/fase1Controller.js
+++ js/fase1Controller.js
@@ -8,28 +8,10 @@ $(document).ready(function() {
 
     if (cartAux) {
         carteraProyectos = JSON.parse(cartAux);
+
+        //Cargar estado actual de la cartera.
+        cargar_criterios();
     } else {
-        /*Abrir archivo JSON on info inicial de la cartera
-        debugger;
-        var myInit = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            mode: 'cors',
-            cache: 'default'
-        }
-        let myReq = new Request("./json/carteraProyectos.json", myInit);
-
-        fetch(myReq)
-            .then(function(resp) {
-                return resp.json();
-            })
-            .then(function(data) {
-                console.log(data);
-            })
-        */
-
         //Alternativa, declarar aquí la cartera
         carteraProyectos = {
             config: {
@@ -65,3 +47,69 @@ $(document).ready(function() {
         localStorage.setItem('carteraProyectos', JSON.stringify(carteraProyectos));
     }
 });
+
+
+//CRITERIOS
+function abrir_nuevoCrit_vtn() {
+    var modal = document.getElementById("nuevoCrit_vtn");
+    modal.style.display = "block";
+}
+
+function cerrar_nuevoCrit() {
+    var modal = document.getElementById("nuevoCrit_vtn");
+    modal.style.display = "none";
+}
+
+function guardar_nuevoCrit() {
+    var modal = document.getElementById("nuevoCrit_vtn");
+    var crit_desc = $("#nuevoCrit_desc").val();
+    var crit_pond = $("#nuevoCrit_pond").val();
+    var crit_tabla = document.getElementById("tabla_criterios");
+    var nueva_fila;
+    var nueva_desc
+    var nueva_pond;
+
+
+    if ((crit_desc != "" && crit_pond != "")) {
+        //Guardar información en caché.
+        carteraProyectos.config.criterios.push({ desc: crit_desc, pond: crit_pond });
+
+        //Añadir información a la tabla.
+        nueva_fila = crit_tabla.insertRow(1);
+        nueva_desc = nueva_fila.insertCell(0);
+        nueva_pond = nueva_fila.insertCell(1);
+
+        nueva_desc.innerHTML = crit_desc;
+        nueva_pond.innerHTML = crit_pond;
+
+        //Limpiar la ventana modal. //NO SE LIMPIA
+        $("#crit_desc").set("");
+        $("#crit_pond").val("");
+        modal.style.display = "none";
+    } else {
+        alert("Debes rellenar los campos");
+    }
+}
+
+function cargar_criterios() {
+    debugger;
+    var crit_tabla = document.getElementById("tabla_criterios");
+    var crit_cached = carteraProyectos.config.criterios;
+    var crit;
+
+    var nueva_fila;
+    var nueva_desc;
+    var nueva_pond;
+
+    for (var i = 0; i < crit_cached.length; i++) {
+        crit = crit_cached[i];
+
+        //Añadir información a la tabla.
+        nueva_fila = crit_tabla.insertRow(1);
+        nueva_desc = nueva_fila.insertCell(0);
+        nueva_pond = nueva_fila.insertCell(1);
+
+        nueva_desc.innerHTML = crit.desc;
+        nueva_pond.innerHTML = crit.pond;
+    }
+}
