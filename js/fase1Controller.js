a7fa61e32b3f78ea947b41a9b75c2d1f55e61106
jaumeferr <36743497+jaumeferr@users.noreply.github.com>
2020-04-24 19:44:56 +0200

    Cambio estructura fase 1
    He canviat basicament tot i ho het fet més fixo a més de fer-ho més estructurat.

--- js/fase1Controller.js
+++ js/fase1Controller.js
@@ -3,6 +3,7 @@ var carteraProyectos;
 
 $(document).ready(function() {
     userLogged = JSON.parse(localStorage.getItem('userLogged'));
+    9
     $("#userInfo").append("User: " + userLogged.user + "\n Role: " + userLogged.role);
     var cartAux = localStorage.getItem('carteraProyectos');
 
@@ -10,7 +11,7 @@ $(document).ready(function() {
         carteraProyectos = JSON.parse(cartAux);
 
         //Cargar estado actual de la cartera.
-        cargar_criterios();
+        //cargar_criterios();
     } else {
         //Alternativa, declarar aquí la cartera
         carteraProyectos = {
@@ -46,9 +47,94 @@ $(document).ready(function() {
 
         localStorage.setItem('carteraProyectos', JSON.stringify(carteraProyectos));
     }
+
+    //Ocultar todas los paneles
+    $("#crit_doc_panel").hide();
+    $("#cal_panel").hide();
+    $("#rec_proc_panel").hide();
+
+    checkPrivileges();
 });
 
+//-----------------------------------------------
+//GENERAL
 
+function checkPrivileges() {
+    var role = userLogged.role;
+    var fase = localStorage.getItem('faseCartera');
+
+    if (role === "rector" && fase === "aprobar_config") {
+        //Ocultar todos los bototes de edición.
+        //Ocultar botoón de enviar.
+        //Mostrar botón de aprobar.
+        //Mostrar botón rechazar
+
+    } else if (role === "rector" && fase != "aprobar config") {
+        //Ocultar todos los botones de edición.
+        //Ocultar botón de enviar.
+        //Ocultar botón de aprobar.
+        //Ocultar botón de rechazar.
+    } else if (role === "cio" && fase === "crear_config") {
+        //Mostrar todos los botones de edición.
+        //Ocultar botón de aprobar.
+        //Ocultar botón de rechazar
+    } else {
+        //Ocultar todos los botones de edición.
+        //Ocultar botón rechazar.
+        //Ocultar botón aprobar.
+        //Ocultar botón enviar.
+    }
+}
+
+//---------------------------------------------
+
+//---------------------------------------------
+//BUTTONS
+
+//TOP
+function onRecProcPanelButtonClick() {
+    $("#crit_doc_panel").hide();
+    $("#cal_panel").hide();
+    $("#rec_proc_panel").show();
+
+}
+
+function onCritDocPanelButtonClick() {
+    $("#rec_proc_panel").hide();
+    $("#cal_panel").hide();
+    $("#crit_doc_panel").show();
+}
+
+function onCalPanelButtonClick() {
+    $("#rec_proc_panel").hide();
+    $("#crit_doc_panel").hide();
+    $("#cal_panel").show();
+}
+
+//BOTTOM
+function onSendConfigButtonClick() {
+
+}
+
+function onApproveConfigButtonClick() {
+
+}
+
+function onRejectConfigButtonClick() {
+
+}
+
+function validate() {
+
+}
+
+function onExitButton() {
+
+}
+
+//--------------------------------------------
+
+/*
 //CRITERIOS
 function abrir_nuevoCrit_vtn() {
     var modal = document.getElementById("nuevoCrit_vtn");
@@ -57,6 +143,8 @@ function abrir_nuevoCrit_vtn() {
 
 function cerrar_nuevoCrit() {
     var modal = document.getElementById("nuevoCrit_vtn");
+    $("#crit_desc").val(" ");
+    $("#crit_pond").val(" ");
     modal.style.display = "none";
 }
 
@@ -83,16 +171,18 @@ function guardar_nuevoCrit() {
         nueva_pond.innerHTML = crit_pond;
 
         //Limpiar la ventana modal. //NO SE LIMPIA
-        $("#crit_desc").set("");
-        $("#crit_pond").val("");
+        $("#crit_desc").val(" ");
+        $("#crit_pond").val(" ");
         modal.style.display = "none";
+
+        //Actualizar localStorage
+        localStorage.setItem('carteraProyectos', JSON.stringify(carteraProyectos));
     } else {
         alert("Debes rellenar los campos");
     }
 }
 
 function cargar_criterios() {
-    debugger;
     var crit_tabla = document.getElementById("tabla_criterios");
     var crit_cached = carteraProyectos.config.criterios;
     var crit;
@@ -112,4 +202,6 @@ function cargar_criterios() {
         nueva_desc.innerHTML = crit.desc;
         nueva_pond.innerHTML = crit.pond;
     }
-}
+}*/
+
+//DOCUMENTACION
